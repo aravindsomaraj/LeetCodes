b21c31441848a25f33f4268ed251595bb3706397
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2022-09-10 08:29:52 +0000

    [LeetCode Sync] Runtime - 60 ms (6.03%), Memory - 19.8 MB (100.00%)

---
+++ Codes/0036-valid-sudoku/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        vector<int> temp (10,1);
+        vector<vector<int>> row (9,temp);
+        vector<vector<int>> col (9,temp);
+        vector<vector<int>> blo (9,temp);
+        int t;
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                if(board[i][j] == '.')
+                    continue;
+                
+                t = board[i][j]-48;
+                
+                if(row[i][t] && col[j][t] && blo[(i/3)*3+(j/3)][t])
+                    row[i][t] = col[j][t] = blo[(i/3)*3+(j/3)][t] = 0;
+                else
+                    return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
