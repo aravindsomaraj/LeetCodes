3c2f12acdcf2cecf5b7099f59f447058fb4f01a9
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2022-09-15 09:09:22 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- Codes/problems/valid_sudoku/solution.cpp
+++ Codes/problems/valid_sudoku/solution.cpp
@@ -3,9 +3,9 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         
         vector<int> temp (10,1);
-        vector<vector<int>> row (9,temp);
-        vector<vector<int>> col (9,temp);
-        vector<vector<int>> blo (9,temp);
+        vector<vector<int>> row (10,temp);
+        vector<vector<int>> col (10,temp);
+        vector<vector<int>> blo (10,temp);
         int t;
         for(int i=0; i<9; i++)
         {
@@ -16,8 +16,8 @@ public:
                 
                 t = board[i][j]-48;
                 
-                if(row[i][t] && col[j][t] && blo[(i/3)*3+(j/3)][t])
-                    row[i][t] = col[j][t] = blo[(i/3)*3+(j/3)][t] = 0;
+                if(row[i][t] && col[t][j] && blo[(i/3)*3+(j/3)][t])
+                    row[i][t] = col[t][j] = blo[(i/3)*3+(j/3)][t] = 0;
                 else
                     return false;
             }
