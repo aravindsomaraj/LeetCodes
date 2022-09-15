b0846fbb74aab56d53e32c2d3ff5648acb78ee13
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2022-09-15 12:30:41 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- Codes/problems/unique_paths/solution.cpp
+++ Codes/problems/unique_paths/solution.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+        
+//         if(m==1 && n==1)
+//             return 1;
+        
+//         vector <vector<int>> dp (m,vector<int> (n,-1));
+        
+//         fun(0,0,dp);
+//         return dp[0][0];
+        
+//     }
+    
+//     int fun(int i, int j,vector<vector<int>>& dp)
+//     {
+//         int m = dp.size();
+//         int n = dp[0].size();
+//         if(i==m-1 && j==n-1)
+//             return 1;
+        
+//         if(i==m || j==n)
+//             return 0;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         return dp[i][j] = fun(i,j+1,dp) + fun(i+1,j,dp);
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
