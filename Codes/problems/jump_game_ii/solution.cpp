8b97917db9ed1694e4c81193d44be58061ab4cda
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2022-10-12 09:06:19 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- Codes/problems/jump_game_ii/solution.cpp
+++ Codes/problems/jump_game_ii/solution.cpp
@@ -2,25 +2,39 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         
+        int n=nums.size();
+        vector<int> dp (n,0);
         
-        int n = nums.size(), mx = nums[0], cr = nums[0];
-        if(n<2)
-            return 0;
-        int j = 1;
-        int i = 0;
+        Solve(nums,dp,n-2);
         
-        while(i<n-1)
+        return dp[0];
+        
+    }
+    
+    void Solve(vector<int>& nums, vector<int>& dp, int i)
+    {
+        if(i<0)
+            return;
+        int max=nums.size();
+        if(nums[i]==0)
+            dp[i]=0;
+        else if(i+nums[i]>=nums.size()-1)
+            dp[i]=1;
+        else
         {
-            mx = max(mx,i+nums[i]);
-            
-            if(cr==i)
+            for(int j=i+1;j<=i+nums[i];j++)
             {
-                j++;
-                cr=mx;
+
+                if(j<nums.size() && dp[j]!=0)
+                {
+                    // update(); //find min jump to target
+                    if((1+dp[j])<max)
+                        max=1+dp[j];
+                }
             }
-            i++;
+            dp[i]=max;
+
         }
-        return j;
-        
+        Solve(nums,dp,i-1);
     }
 };
