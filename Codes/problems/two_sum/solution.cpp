934d13af0aa9895ad0b8838a8bc2eaf5933c3cb3
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2022-11-29 15:30:23 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- Codes/problems/two_sum/solution.cpp
+++ Codes/problems/two_sum/solution.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int> a;
-        unordered_map<int,int> m;
+        unordered_map<int,int> mp;
         
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<nums.size();i++)
         {
-            if(m.find(target-nums[i]) != m.end())
+            if(mp.find(target-nums[i])!=mp.end())
             {
-                a.push_back(m[target-nums[i]]);
+                a.push_back(mp[target-nums[i]]);
                 a.push_back(i);
                 return a;
             }
-            m[nums[i]] = i;
+            mp[nums[i]] = i;
         }
         return a;
     }
