953476a60366f3e903988f4584d286594f0bbfd3
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-19 05:47:23 +0000

    [LeetCode Sync] Runtime - 11 ms (52.13%), Memory - 7.8 MB (99.99%)

--- Codes/0013-roman-to-integer/solution.cpp
+++ Codes/0013-roman-to-integer/solution.cpp
@@ -1,65 +1,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        
-        unordered_map<char,int> mp;
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
-        
+
+        unordered_map<char,int> mp = { { 'I' , 1 },
+                                        { 'V' , 5 },
+                                        { 'X' , 10 },
+                                        { 'L' , 50 },
+                                        { 'C' , 100 },
+                                        { 'D' , 500 },
+                                        { 'M' , 1000 } };
         int num=0;
-        for(int i=0; s[i]!='\0'; i++)
+        for(int i=0; i<s.size();i++)
         {
             char ch=s[i];
-            switch(ch)
+            if(s[i+1]!='\0' && mp[ch]<mp[s[i+1]])
             {
-                case 'I':   if(s[i+1]=='V')
-                            {
-                                num+=4;
-                                i++;
-                            }
-                            else if(s[i+1]=='X')
-                            {
-                                num+=9;
-                                i++;
-                            }    
-                            else
-                                num+=1;
-                            break;
-                case 'X':   if(s[i+1]=='L')
-                            {
-                                num+=40;
-                                i++;
-                            }
-                            else if(s[i+1]=='C')
-                            {
-                                num+=90;
-                                i++;
-                            }    
-                            else
-                                num+=10;
-                            break;
-                case 'C':   if(s[i+1]=='D')
-                            {
-                                num+=400;
-                                i++;
-                            }
-                            else if(s[i+1]=='M')
-                            {
-                                num+=900;
-                                i++;
-                            }    
-                            else
-                                num+=100;
-                            break;
-                default: num+=mp[ch];
+                num+=mp[s[i+1]]-mp[s[i]];
+                i++;
+            }
+            else
+            {
+                num+=mp[s[i]];
             }
         }
-        
         return num;
     }
 };
