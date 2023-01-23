fcfa33a9e80ac2992f7f5e8385dbe8d8115a1d6c
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-23 07:19:39 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (cpp)

--- Codes/problems/pacific_atlantic_water_flow/solution.cpp
+++ Codes/problems/pacific_atlantic_water_flow/solution.cpp
@@ -1,65 +1,56 @@
 class Solution {
 public:
-    int m,n;
-    
-    int s(vector<vector<int>>& ht, int i, int j, vector<vector<int>>& oc)
+
+    bool Solve(vector<vector<int>>& hi, vector<vector<bool>>& oc, int i, int j, int h)
     {
-        if(i<0 || j<0 || i==m || j==n || ht[i][j]==100004)
-            return 0;
+        if(i<0 || j<0 || i==hi.size() || j==hi[0].size() || hi[i][j]==100001 || hi[i][j]>h)
+            return false;
         
-        if(oc[i][j]==1)
-            return oc[i][j];
-        
-        // cout << i << "," << j << endl;
-        
-        int a=0;
-        int k = ht[i][j];
-        ht[i][j]=100004;
-        
-        if (i>0 && ht[i-1][j]<=k)   
-            a+= s(ht,i-1,j,oc);
-        if (j>0 && ht[i][j-1]<=k)   
-            a+= s(ht,i,j-1,oc);
-        if (i<m-1 && ht[i+1][j]<=k) 
-            a+= s(ht,i+1,j,oc);
-        if (j<n-1 && ht[i][j+1]<=k) 
-            a+= s(ht,i,j+1,oc);
-        
+        if(oc[i][j])
+            return true;
+
+        int val = hi[i][j];
+        hi[i][j]= 100001;
+        bool a = false;
+
+        a = Solve(hi,oc,i+1,j,val) || a;
+        a = Solve(hi,oc,i,j-1,val) || a;
+        a = Solve(hi,oc,i,j+1,val) || a;
+        a = Solve(hi,oc,i-1,j,val) || a;
+
         if(a)
-            oc[i][j]=1;
-        
-        ht[i][j]=k;
-        return oc[i][j];
-        
+            oc[i][j]=a;
+            
+        hi[i][j]=val;
+        return a;
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        
-        m = heights.size();
-        n = heights[0].size();
-        
-        vector<vector<int>> atl(m, vector<int> (n,0));
-        vector<vector<int>> pac(m, vector<int> (n,0));
-        
-        for(int i=0;i<m;i++)
+
+        int m = heights.size();
+        int n = heights[0].size();
+
+        vector<vector<bool>> pac (m,vector<bool> (n,false));
+        vector<vector<bool>> atl (m,vector<bool> (n,false));
+
+        for(int i=0; i<n; i++)
         {
-            pac[i][0]=1;
-            atl[i][n-1]=1;
+            pac[0][i]=true;atl[m-1][i]=true;
         }
-        for(int i=0;i<n;i++)
+        for(int i=0; i<m; i++)
         {
-            pac[0][i]=1;
-            atl[m-1][i]=1;
+            pac[i][0]=true;atl[i][n-1]=true;
         }
-        vector<vector<int>> res;
+        
+        vector<vector<int>> ans;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(s(heights,i,j,pac) && s(heights,i,j,atl))
-                    res.push_back({i,j});
+                if(Solve(heights,pac,i,j,100001) && Solve(heights,atl,i,j,100001))
+                    ans.push_back({i,j});
             }
         }
-        
-        return res;
+
+        return ans;
     }
 };
