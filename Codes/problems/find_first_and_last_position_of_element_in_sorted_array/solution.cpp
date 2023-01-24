463c667b50001fc38e094c02c3d93a78442fd4b3
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-24 18:23:36 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (cpp)

--- Codes/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
+++ Codes/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
@@ -1,54 +1,17 @@
 class Solution {
 public:
-    
-    void binarySearch(vector<int>& nums, int l, int r, int key, int *a)
-    {
-        if(l>r)
-            return ;
-        
-        int mid = (l+r)/2;
-        
-        if(nums[mid]==key)
-        {
-            *a = mid;
-            binarySearch(nums,l,mid-1,key,a);
-        }
-        
-        else if(nums[mid]>key)
-            binarySearch(nums,l,mid-1,key,a);
-    
-        else
-            binarySearch(nums,mid+1,r,key,a);
-        
-    }
-    void binarySearch2(vector<int>& nums, int l, int r, int key, int *a)
-    {
-        if(l>r)
-            return ;
-        
-        int mid = (l+r)/2;
-        
-        if(nums[mid]==key)
-        {
-            *a = mid;
-            binarySearch2(nums,mid+1,r,key,a);
-        }
-        
-        else if(nums[mid]>key)
-            binarySearch2(nums,l,mid-1,key,a);
-    
-        else
-            binarySearch2(nums,mid+1,r,key,a);
-        
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        vector<int> a (2,-1);
-        
-        binarySearch(nums,0,nums.size()-1,target,&a[0]);
-        binarySearch2(nums,0,nums.size()-1,target,&a[1]);
-        
-        return a;
-        
+        int f=-1,l=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                if(f==-1)
+                    f=i;
+                l=i;
+            }
+        }
+        return {f,l};
     }
 };
