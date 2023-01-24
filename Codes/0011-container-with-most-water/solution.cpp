3871870648e452ed1d9f56e9c6d95086caac1f68
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-24 13:14:52 +0000

    [LeetCode Sync] Runtime - 98 ms (5.47%), Memory - 59.1 MB (99.75%)

--- Codes/0011-container-with-most-water/solution.cpp
+++ Codes/0011-container-with-most-water/solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    
-    int Area(vector<int>& height , int l, int r)
-    {
-        int m=0;
-        if(l>=r)
-            return 0;
-        
-        int area = (r-l)*min(height[l],height[r]);
-        
-        if(area > m)
-            m = area;
-        
-        if(height[l]>height[r])
-            return max(m,Area(height,l,r-1));
-        else
-            return max(m,Area(height,l+1,r));
-        
-        
-    }
     int maxArea(vector<int>& height) {
-        
-        int max = Area(height,0,height.size()-1);
-        
-        return max;
-        
+
+        int l=0,r=height.size()-1;
+        int maxa=0;
+
+        while(l<r)
+        {
+            int area = (r-l)*min(height[l],height[r]);
+            maxa=max(maxa,area);
+
+            if(height[l]<height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxa;
     }
 };
