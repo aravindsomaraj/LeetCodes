399c0b79afd61a9d7d0afd079955498185ed81cd
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 11:18:42 +0000

    [LeetCode Sync] Runtime - 186 ms (5.56%), Memory - 70.3 MB (9.96%)

--- Codes/0053-maximum-subarray/solution.cpp
+++ Codes/0053-maximum-subarray/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& A) {
-        
-        int n = A.size();
-        int dp[n];//dp[i] means the maximum subarray ending with A[i];
-        dp[0] = A[0];
-        int maxx = dp[0];
-        
-        for(int i = 1; i < n; i++){
-            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            maxx = max(maxx, dp[i]);
+    int maxSubArray(vector<int>& nums) {
+
+        vector<int> dp (nums.size(),0);dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            dp[i]=max(dp[i-1]+nums[i],nums[i]);
         }
-        
-        return maxx;
-        
+        return *max_element(dp.begin(),dp.end());
     }
 };
