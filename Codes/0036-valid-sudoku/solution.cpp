19d3ffd4ef05784607059f3566d80fa496abe68a
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 06:58:29 +0000

    [LeetCode Sync] Runtime - 41 ms (6.03%), Memory - 19.9 MB (100.00%)

--- Codes/0036-valid-sudoku/solution.cpp
+++ Codes/0036-valid-sudoku/solution.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
-        vector<int> temp (10,1);
-        vector<vector<int>> row (10,temp);
-        vector<vector<int>> col (10,temp);
-        vector<vector<int>> blo (10,temp);
-        int t;
-        for(int i=0; i<9; i++)
+
+        vector<vector<int>> row (9,vector<int> (9,0));
+        vector<vector<int>> col (9,vector<int> (9,0));
+        vector<vector<int>> blk (9,vector<int> (9,0));
+
+        for(int i=0;i<9;i++)
         {
-            for(int j=0; j<9; j++)
+            for(int j=0;j<9;j++)
             {
-                if(board[i][j] == '.')
+                if(board[i][j]=='.')
                     continue;
-                
-                t = board[i][j]-48;
-                
-                if(row[i][t] && col[t][j] && blo[(i/3)*3+(j/3)][t])
-                    row[i][t] = col[t][j] = blo[(i/3)*3+(j/3)][t] = 0;
-                else
+                int n = board[i][j]-48;n=n-1;
+                if(row[n][i] || col[n][j] || blk[n][(i/3)+3*(j/3)])
                     return false;
+                    
+                row[n][i]=1;col[n][j]=1;blk[n][(i/3)+3*(j/3)]=1;
+                
             }
         }
-        
         return true;
-        
     }
 };
