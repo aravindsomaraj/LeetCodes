258ba902ca7e6d263c014e961022a4346f18ba06
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 07:20:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

--- Codes/0046-permutations/solution.cpp
+++ Codes/0046-permutations/solution.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    
+    vector<vector<int>> ans;
+    void permute(vector<int>& nums, int i)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        // cout << "Test No: #" << i << endl;
+        for(int j=i;j<nums.size();j++)
+        {
+            swap(nums[i],nums[j]);
+            permute(nums,i+1);
+            swap(nums[i],nums[j]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
-     vector<vector<int>> a;
-        sort(nums.begin(),nums.end());
-        a.push_back(nums);
-        while(next_permutation(nums.begin(), nums.end()))
-          {
-              a.push_back(nums);
-          }
-              
+
+        permute(nums,0);
+        return ans;
         
-        return a;
     }
 };
