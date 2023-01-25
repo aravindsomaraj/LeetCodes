3179ff857c9880553d93a6bcad67743e7b233a80
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 06:45:08 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- Codes/problems/search_in_rotated_sorted_array/solution.cpp
+++ Codes/problems/search_in_rotated_sorted_array/solution.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
-        int n = nums.size();
+
+        int n=nums.size();
         int l=0,r=n-1;
-        
+
         while(l<=r)
         {
-            int mid = (l+r)/2;
+            int mid=(l+r)/2;
             if(nums[mid]==target)
                 return mid;
-            
             if(nums[l]<=nums[mid])
             {
-                if(target >= nums[l] && target < nums[mid])
-                    r = mid - 1;
+                if(target>=nums[l] && target<nums[mid])
+                    r=mid-1;
                 else
-                    l = mid + 1;
+                    l=mid+1;
             }
             else
             {
-                if(target <= nums[r] && target > nums[mid])
-                    l = mid + 1;
+                if(target>nums[mid]&&target<=nums[r])
+                    l=mid+1;
                 else
-                    r = mid - 1;
+                    r=mid-1;
             }
         }
-        
         return -1;
     }
 };
