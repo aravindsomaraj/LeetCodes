102527ed3dcc5f8a5fe167c9a34b79bf82f50d55
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 11:47:41 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- Codes/problems/jump_game/solution.cpp
+++ Codes/problems/jump_game/solution.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
         int n=nums.size();
-        vector<int> dp (n,0);
-        dp[n-1]=1;
-        
-        Solve(nums,dp,n-1);
-        
-        return dp[0];
-        
-    }
-    
-    void Solve(vector<int>& nums, vector<int>& dp, int i)
-    {
-        if(i<0)
-            return ;
-        
-        for(int j=i+1;j<=i+nums[i];j++)
+        vector<bool> dp (n,false);
+        dp[n-1]=true;
+        for(int i=n-2; i>=0; i--)
         {
-            if(j<nums.size() && dp[j])
+            for(int j=i+1;j<=(i+nums[i]);j++)
             {
-                
-                dp[i]=1;
-                break;
+                if(dp[j])
+                {
+                    dp[i]=true;break;
+                }
             }
-            
         }
-        cout << dp[i];
-        Solve(nums,dp,i-1);
-        
+        return dp[0];
     }
 };
