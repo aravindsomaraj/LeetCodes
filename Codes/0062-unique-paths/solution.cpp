b1c9f012ac5c9c9e654828c6ddbe860516b7957b
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-01-25 12:19:59 +0000

    [LeetCode Sync] Runtime - 2 ms (42.70%), Memory - 6.6 MB (100.00%)

--- Codes/0062-unique-paths/solution.cpp
+++ Codes/0062-unique-paths/solution.cpp
@@ -1,63 +1,22 @@
-// class Solution {
-// public:
-//     int uniquePaths(int m, int n) {
-        
-//         if(m==1 && n==1)
-//             return 1;
-        
-//         vector <vector<int>> dp (m,vector<int> (n,-1));
-        
-//         fun(0,0,dp);
-//         return dp[0][0];
-        
-//     }
-    
-//     int fun(int i, int j,vector<vector<int>>& dp)
-//     {
-//         int m = dp.size();
-//         int n = dp[0].size();
-//         if(i==m-1 && j==n-1)
-//             return 1;
-        
-//         if(i==m || j==n)
-//             return 0;
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
-//         return dp[i][j] = fun(i,j+1,dp) + fun(i+1,j,dp);
-        
-//     }
-// };
-
-
-
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        
-        if(m==1 && n==1)
-            return 1;
-        
-        vector<vector<int>> dp (m,vector<int> (n,-1));
-        
-        fun(m-1,n-1,dp);
-        
-        return dp[m-1][n-1];
-        
-    }
-    
-    int fun(int i, int j, vector<vector<int>>& dp)
+    int Solve(int m, int n, int i, int j, vector<vector<int>>& dp)
     {
-        if(i==0 && j==0)
-            return 1;
-        
-        if(i<0 || j<0)
+        if(i==m || j==n)
             return 0;
-        
+        if(i==m-1 && j==n-1)
+            return 1;
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        return dp[i][j] = fun(i,j-1,dp) + fun(i-1,j,dp);
-        
+        dp[i][j]=Solve(m,n,i+1,j,dp)+Solve(m,n,i,j+1,dp);
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        if(m==1 && n==1)
+            return 1;
+        vector<vector<int>> dp (m, vector<int> (n,-1));
+        Solve(m,n,0,0,dp);
+        return dp[0][0];
     }
 };
-
