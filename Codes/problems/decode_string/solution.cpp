034d266d672c49aca513fdeb4959fa030913fbf5
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-02-13 05:07:02 +0000

    Sync LeetCode submission - Decode String (cpp)

---
+++ Codes/problems/decode_string/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string Solve(string& s, int &i)
+    {
+        string res;
+
+        while(i<s.size() && s[i]!=']')
+        {
+            if(!isdigit(s[i]))
+                res+=s[i++];
+            else
+            {
+                int n=0;
+                while(i<s.size() && isdigit(s[i]))
+                    n=n*10 + s[i++]-'0';
+                
+                i++;
+                string t = Solve(s,i);
+                i++;
+
+                while(n-- > 0)
+                    res+=t;
+            }
+        }
+        return res;
+    }
+    string decodeString(string s) {
+        
+        int i=0;
+        return Solve(s,i);
+    }
+};
