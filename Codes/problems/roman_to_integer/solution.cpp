88cd80cfd343e43a66658e50b78d85f3ff99a4e0
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-04-30 19:43:21 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- Codes/problems/roman_to_integer/solution.cpp
+++ Codes/problems/roman_to_integer/solution.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     int romanToInt(string s) {
-
-        unordered_map<char,int> mp = { { 'I' , 1 },
-                                        { 'V' , 5 },
-                                        { 'X' , 10 },
-                                        { 'L' , 50 },
-                                        { 'C' , 100 },
-                                        { 'D' , 500 },
-                                        { 'M' , 1000 } };
+        unordered_map<char,int> mp { {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
         int num=0;
-        for(int i=0; i<s.size();i++)
+        int temp=0;
+        for(int i=0;s[i]!='\0';i++)
         {
-            char ch=s[i];
-            if(s[i+1]!='\0' && mp[ch]<mp[s[i+1]])
+            char ch = s[i];
+            temp = i+1;
+            switch(ch)
             {
-                num+=mp[s[i+1]]-mp[s[i]];
-                i++;
+                case 'I': 
+                    if(s[temp]=='V' || s[temp]=='X')
+                    {
+                        num += mp[s[temp]] - mp[ch];
+                        i = temp;
+                    }
+                    else
+                        num += mp[ch];
+                    break;
+                case 'X':
+                    if(s[temp]=='L' || s[temp]=='C')
+                    {
+                        num += mp[s[temp]] - mp[ch];
+                        i = temp;
+                    }
+                    else
+                        num += mp[ch];
+                    break;
+                case 'C':
+                    if(s[temp]=='D' || s[temp]=='M')
+                    {
+                        num += mp[s[temp]] - mp[ch];
+                        i = temp;
+                    }
+                    else
+                        num += mp[ch];
+                    break;
+                default: num += mp[ch];
             }
-            else
-            {
-                num+=mp[s[i]];
-            }
+            cout << num << s[temp] << ch << endl;
         }
         return num;
     }
