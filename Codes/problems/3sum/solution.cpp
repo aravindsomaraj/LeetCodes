c599239e179064e4c6023ba1de89d61ec5970dbc
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-28 18:06:23 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- Codes/problems/3sum/solution.cpp
+++ Codes/problems/3sum/solution.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        sort(nums.begin(),nums.end());
+        
         int n=nums.size();
-        if(n<3 || nums[0]>0)
-            return {};
-        vector<vector<int>> ans;
-        for(int i=0;i<n;i++)
+        sort(nums.begin(), nums.end());
+        set<vector<int>> set;
+        vector<vector<int>> output;
+        for(int i=0;i<n-2;i++)
         {
-            if(nums[i]>0)
-                break;
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
-            
-            int low=i+1,high=n-1;
+            int low = i+1,high=n-1;
             while(low<high)
             {
-                int sum=nums[i]+nums[low]+nums[high];
-                if(sum>0)
-                    high--;
-                else if(sum<0)
+                if(nums[low]+nums[high]+nums[i] < 0)
                     low++;
+                else if(nums[low]+nums[high]+nums[i] > 0)
+                    high--;
                 else
                 {
-                    ans.push_back({nums[i],nums[low],nums[high]});
-                    int loc=nums[low];int hoc=nums[high];
-                    while(low<high&&nums[low]==loc)
-                        low++;
-                    while(low<high&&nums[high]==hoc)
-                        high--;
-                }   
+                    set.insert({nums[i],nums[low],nums[high]});
+                    low++;
+                    high--;
+                }
             }
-
         }
-        return ans;
+        for(auto& v:set) output.push_back(v);
+        return output;
     }
 };
