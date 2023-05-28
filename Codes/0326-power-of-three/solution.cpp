f8162950a8cdb74e3ddc4597159d46ab1f6b31ba
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-28 10:00:26 +0000

    [LeetCode Sync] Runtime - 24 ms (10.38%), Memory - 5.9 MB (100.00%)

--- Codes/0326-power-of-three/solution.cpp
+++ Codes/0326-power-of-three/solution.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
-        if(n<=0)
-            return false;
-        int d=0;
-        while(n>1 && d==0)
-        {
-            d=n%3;
-            n=n/3;
-        }
-        
-        if(n==1 && d==0)
-            return true;
-        return false;
-        
+	return n > 0 && 1162261467 % n == 0;
+	// pow(3, floor(log(INT_MAX)/log(3))) = 1162261467 
     }
 };
