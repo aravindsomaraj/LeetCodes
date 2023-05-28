78945e91518172b1eb7ae7fe0d1d40f01ec1bfc4
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-28 15:26:45 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- Codes/problems/reverse_integer/solution.cpp
+++ Codes/problems/reverse_integer/solution.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     int reverse(int x) {
 
-        int f=0;
-        (x<0)?f=-1:f=1;
-        if(x==INT_MIN)
-            return 0;
-        x*=f;
-        int temp=x;
-        int rev=0;
-        while(temp>=10)
+        if(!x) return 0;
+
+        int y = abs(x),rev=0;
+
+        while(y>0)
         {
-            int dig=temp%10;
-            temp/=10;
-            rev=rev*10+dig;
+            int d=y%10;
+            if(rev <= (INT_MAX)/10)
+                rev = rev*10 + d;
+            else return 0;
+            y/=10;
         }
-        if(rev>(INT_MAX)/10)
-            return 0;
-        else
-        {
-            return (f*(rev*10+temp));
-        }
+        x = x>0? rev:-rev;
+        return x;
     }
 };
