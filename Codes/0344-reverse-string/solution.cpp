a6f16616dcd7af2c422b0cbb26264276464ff104
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-28 10:02:04 +0000

    [LeetCode Sync] Runtime - 27 ms (5.03%), Memory - 23.2 MB (100.00%)

--- Codes/0344-reverse-string/solution.cpp
+++ Codes/0344-reverse-string/solution.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+
+        int l=0,r=s.size()-1;
+        while(l<=r)
+        {
+            swap(s.at(l++),s.at(r--));
+        }
         
-        reverse(s.begin(),s.end());
     }
 };
