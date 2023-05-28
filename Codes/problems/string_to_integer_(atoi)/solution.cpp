fc6c6c313d827bf626f32eaf6946a0a404a1ea94
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-28 16:54:37 +0000

    Sync LeetCode submission - String to Integer (atoi) (cpp)

--- Codes/problems/string_to_integer_(atoi)/solution.cpp
+++ Codes/problems/string_to_integer_(atoi)/solution.cpp
@@ -1,54 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        
+        int n = s.size();
+        int i = 0;
+        while(i<n && s[i]==' ')
+            i++;
+        
+        int f = 0;
+        if(s[i]=='-') f = -1;
+        else if(s[i]=='+') f = 1;
+        !f? f=1: i++;
         int num=0;
-        long numtrial=0;
-        int i=0;
-        while(s[i]==' ')
+        while(i<n && isdigit(s[i]))
         {
-            i++;
-        }
-        int flag=1;
-        if(s[i]=='-')
-        {
-            i++;
-            flag=0;
-        }
-        else if(s[i]=='+')
-        {
-            i++;
-        }
-        int f=0;
-        while(s[i]>='0' && s[i]<='9')
-        {
-            if(numtrial<=INT_MAX)
-                numtrial=numtrial*10 + (s[i]-48);
-            // else
-            // {
-            //     f=1;
-            //     num=INT_MAX;
-            // }
+            int d = s[i++]-'0';
+            if(num > INT_MAX/10 || (num==INT_MAX/10 && d>7))
+                return f>0? INT_MAX:INT_MIN;
             
-            i++;
+            num = num*10 + d;
         }
-        
-        if(flag==0)
-        {
-            numtrial=-numtrial;
-            // if(f==1)
-            //     num=num-1;
-        }
-        
-        if(numtrial>INT_MAX)
-            num=INT_MAX;
-        else if(numtrial<INT_MIN)
-            num=INT_MIN;
-        else
-            num=(int)numtrial;
-        return num;
-        
-        
-        
-        
+        return f*num;
+
     }
 };
