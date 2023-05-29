a0233d674d9056d42d9025988096147dc94615ca
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-29 11:57:57 +0000

    [LeetCode Sync] Runtime - 6 ms (5.97%), Memory - 6.9 MB (100.00%)

--- Codes/0048-rotate-image/solution.cpp
+++ Codes/0048-rotate-image/solution.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
+
+        int n = matrix.size();
         reverse(matrix.begin(),matrix.end());
-        
-        for(int i=0; i<matrix.size();i++)
-        {
-            for(int j=i+1; j<matrix[i].size();j++)
-            {
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        
+
+        for(int i=0;i<n;i++)for(int j=i;j<n;j++)swap(matrix[i][j],matrix[j][i]);
     }
 };
