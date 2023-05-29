cf2c471e4a34fd4d5a2a0d76845284e7c80a4a16
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-29 11:34:14 +0000

    [LeetCode Sync] Runtime - 4 ms (52.66%), Memory - 6.8 MB (100.00%)

--- Codes/0038-count-and-say/solution.cpp
+++ Codes/0038-count-and-say/solution.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     string Solve(string s)
     {
-        char count='0';
-        char ch=s[0];string ans="";
-        for(int i=0;s[i]!='\0';i++)
+        if(s=="")
+            return "1";
+        int c=0; string ans="";
+        for(int i=0;s[i]!='\0';)
         {
-            if(s[i]!=ch)
+            c=0;
+            char temp = s[i];
+            while(i<s.size() && temp==s[i])
             {
-                ans=ans+count+ch;
-                ch=s[i];
-                count='1';
+                c++;i++;
             }
-            else
-                count=count+1;
+            ans += to_string(c) + temp;
         }
-        ans=ans+count+ch;
         return ans;
     }
     string countAndSay(int n) {
-        string s="1";
-        for(int i=1;i<n;i++)
+
+        string s="";
+        for(int i=1;i<=n;i++)
         {
-            s=Solve(s);
+            s = Solve(s);
         }
         return s;
     }
