099f0463ebcc41f2489eae16aeaafd23391df93a
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-29 07:02:58 +0000

    [LeetCode Sync] Runtime - 3 ms (60.52%), Memory - 13.8 MB (68.02%)

--- Codes/0022-generate-parentheses/solution.cpp
+++ Codes/0022-generate-parentheses/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    void Solve(int n, string s, int i, int j, vector<string>& ans)
+    vector<string> ans;
+    void Solve(int i, int n, string s)
     {
+        if(2*i < s.size())
+            return;
         if(i==n)
         {
-            while(s.size()!=2*n)
-            {
-                s = s + ")";
-            }
+            while(s.size()<(2*n)) s=s+")";
             ans.push_back(s);
             return;
         }
-        if(j>i)
-            return ;
-        
-        Solve(n,s+"(",i+1,j,ans);
-        Solve(n,s+")",i,j+1,ans);
-        
-        
+
+        Solve(i+1,n,s+"(");
+        Solve(i,n,s+")");
     }
     vector<string> generateParenthesis(int n) {
-        
-        vector<string> ans;
-        Solve(n,"",0,0,ans);
+        Solve(0,n,"");
         return ans;
-        
     }
 };
