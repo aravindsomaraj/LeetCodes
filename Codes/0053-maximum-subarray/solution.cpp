76a592f402f8d73b27477fb30f56d859d1bbaec2
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-30 09:00:20 +0000

    [LeetCode Sync] Runtime - 131 ms (5.56%), Memory - 67.7 MB (99.87%)

--- Codes/0053-maximum-subarray/solution.cpp
+++ Codes/0053-maximum-subarray/solution.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-
-        vector<int> dp (nums.size(),0);dp[0]=nums[0];
-        for(int i=1;i<nums.size();i++)
-        {
-            dp[i]=max(dp[i-1]+nums[i],nums[i]);
+        // Initialize maxSum as an integer that cannot store any value below the limit...
+        int maxSum = INT_MIN;
+        // Initialize maxSum...
+        int maxSumSoFar = 0;
+        // Traverse all the element through the loop...
+        for(int i = 0; i < nums.size(); i++){
+            // Keep adding the current value...
+            maxSumSoFar += nums[i];
+            // Update maxSum to maxSum and maxSumSoFar...
+            if(maxSum < maxSumSoFar){
+                maxSum = maxSumSoFar;
+            }
+            // if maxSumSoFar is less than 0 then update it to 0...
+            if(maxSumSoFar < 0){
+                maxSumSoFar = 0;
+            }
         }
-        return *max_element(dp.begin(),dp.end());
+        return maxSum;      // Return the contiguous subarray which has the largest sum...
     }
 };
