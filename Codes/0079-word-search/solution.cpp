1c521149c15abca667953bd59f61c98c29e7bf7a
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-05-31 13:02:56 +0000

    [LeetCode Sync] Runtime - 456 ms (64.15%), Memory - 7.9 MB (100.00%)

--- Codes/0079-word-search/solution.cpp
+++ Codes/0079-word-search/solution.cpp
@@ -1,43 +1,33 @@
 class Solution {
-public: 
-    bool Solve(vector<vector<char>>& board, string word, int i, int j, int k)
+public:
+    int m,n;string s;
+    bool dfs(vector<vector<char>>& board, int i, int j, int k)
     {
-        if(k==word.size())
-            return true;
-        
-        if(i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[k] || board[i][j]=='.')
-            return false;
-        
-        bool abc=false;
-        char ch = board[i][j];
-        board[i][j]='.';
-        
-        abc = Solve(board,word,i+1,j,k+1) || Solve(board,word,i-1,j,k+1) || Solve(board,word,i,j+1,k+1) || Solve(board,word,i,j-1,k+1);
-        // if(i<board.size()-1)
-        //     abc = abc || Solve(board,word,i+1,j,k+1);
-        // if(i>0)
-        //     abc = abc || Solve(board,word,i-1,j,k+1);
-        // if(j<board[0].size()-1)
-        //     abc = abc || Solve(board,word,i,j+1,k+1);
-        // if(j>0)
-        //     abc = abc || Solve(board,word,i,j-1,k+1);
-        
+        if(k==s.size()) return true;
+        if(i==m || j==n || i<0 || j<0 || board[i][j]=='.' || board[i][j]!=s[k]) return false;
+
+        char ch = board[i][j];board[i][j]='.';
+        bool f = (
+            dfs(board,i+1,j,k+1) ||
+            dfs(board,i-1,j,k+1) ||
+            dfs(board,i,j+1,k+1) ||
+            dfs(board,i,j-1,k+1)
+        );
         board[i][j]=ch;
-        return abc;
+        return f;
     }
     bool exist(vector<vector<char>>& board, string word) {
-       
-        
-        for(int i=0;i<board.size();i++)
+        m = board.size(); n = board[0].size(); s=word;
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for(int j=0;j<n;j++)
             {
-                if(Solve(board,word,i,j,0))
-                    return true;
-                
+                if(board[i][j]==word[0])
+                {
+                    if(dfs(board,i,j,0)) return true;
+                }
             }
         }
         return false;
-        
     }
 };
