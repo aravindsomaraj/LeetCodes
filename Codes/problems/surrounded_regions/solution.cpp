05ec2aca9251c725c198f07e598ad2a845ba14ab
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-02 12:05:19 +0000

    Sync LeetCode submission - Surrounded Regions (cpp)

--- Codes/problems/surrounded_regions/solution.cpp
+++ Codes/problems/surrounded_regions/solution.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
+    int m,n;
     void dfs(vector<vector<char>>& board, int i, int j)
     {
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='V' || board[i][j]=='X')
+        if(i==m || j==n || i<0 || j<0 || board[i][j]!='O')
             return;
-
         
-        board[i][j]='V';
-        dfs(board,i-1,j);
-        dfs(board,i+1,j);
-        dfs(board,i,j-1);
+        board[i][j]='.';
         dfs(board,i,j+1);
-
+        dfs(board,i,j-1);
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
     }
     void solve(vector<vector<char>>& board) {
+        m=board.size();n=board[0].size();
 
-        int m = board.size();
-        int n = board[0].size();
-
-        for(int i=0; i<m; i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(i*j==0 || i==m-1 || j==n-1)
-                {
-                    if(board[i][j]=='O')
-                        dfs(board,i,j);
-                }
-            }
+            dfs(board,i,0); dfs(board,i,n-1);
         }
-
-        for(int i=0; i<m; i++)
+        for(int j=0;j<n;j++)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(board[i][j]=='O')
-                    board[i][j]='X';
-                else if(board[i][j]=='V')
-                    board[i][j]='O';
-            }
+            dfs(board,0,j); dfs(board,m-1,j);
         }
-        
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)
+        {
+            if(board[i][j]=='.') board[i][j]='O';
+            else board[i][j]='X';
+        }
     }
 };
