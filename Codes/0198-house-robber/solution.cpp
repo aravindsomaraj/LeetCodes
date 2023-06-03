fd023777a374278a683e5f1e90ed24ce74469eb4
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-03 14:46:48 +0000

    [LeetCode Sync] Runtime - 5 ms (32.52%), Memory - 7.9 MB (100.00%)

--- Codes/0198-house-robber/solution.cpp
+++ Codes/0198-house-robber/solution.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
-    void Solve(vector<int>& nums, int i, vector<int>& dp)
-    {
-        if(i<0)
-            return;
-        
-        
-        dp[i] = max(dp[i+2]+nums[i], dp[i+1]);
-        Solve(nums,i-1,dp);
-        
-            
-        
-    }
     int rob(vector<int>& nums) {
-        
-        vector<int> dp (nums.size()+1, 0);
-    
-        if(nums.size()==0)
-            return 0;
-        
-        dp[nums.size()-1]=nums[nums.size()-1];
-        Solve(nums,nums.size()-2,dp);
-        
-        return dp[0];
-        
+        int n=nums.size();
+        vector<int> dp (n+1,0);
+        dp[0]=0;dp[1]=nums[0];
+        for(int i=1;i<n;i++) dp[i+1]=max(dp[i-1]+nums[i],dp[i]);
+        return dp[n];
     }
 };
