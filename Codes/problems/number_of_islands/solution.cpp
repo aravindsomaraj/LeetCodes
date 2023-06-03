ac4a4a4987be45459aeac912208adb8a09428ac0
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-03 14:57:55 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- Codes/problems/number_of_islands/solution.cpp
+++ Codes/problems/number_of_islands/solution.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>& dp, int i, int j)
+    int c,m,n;
+    void dfs(vector<vector<char>>& grid, int i, int j)
     {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || dp[i][j]==1 || grid[i][j]=='0')
+        if(i==m || j==n || i<0 || j<0 || grid[i][j]=='.' || grid[i][j]=='0')
             return;
-
-        dp[i][j]=1;
-        if(grid[i][j]=='1')
-        {
-            dfs(grid,dp,i+1,j);
-            dfs(grid,dp,i-1,j);
-            dfs(grid,dp,i,j+1);
-            dfs(grid,dp,i,j-1);
-        }
+        
+        grid[i][j]='.';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
-
-        int m = grid.size();
-        int n = grid[0].size();
-        int c=0;
-        vector<vector<int>> dp (m, vector<int> (n,0));
-
-        for(int i=0; i<m; i++)
+        c=0;
+        m=grid.size();n=grid[0].size();
+        for(int i=0;i<m;i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j=0;j<n;j++)
             {
-                if(dp[i][j]!=1 && grid[i][j]=='1')
+                if(grid[i][j]=='1')
                 {
-                    c++;
-                    dfs(grid,dp,i,j);
+                    c++; dfs(grid,i,j);
                 }
             }
         }
-
         return c;
     }
 };
