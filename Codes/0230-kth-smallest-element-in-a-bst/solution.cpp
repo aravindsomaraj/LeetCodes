3275c0e3faac89aee87577159a078e84f048d549
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-05 11:24:55 +0000

    [LeetCode Sync] Runtime - 21 ms (5.47%), Memory - 27.3 MB (5.38%)

--- Codes/0230-kth-smallest-element-in-a-bst/solution.cpp
+++ Codes/0230-kth-smallest-element-in-a-bst/solution.cpp
@@ -11,20 +11,31 @@
  */
 class Solution {
 public:
-    void Solve(TreeNode* root,vector<int>& v)
-    {
-        if(root==0)
-            return;
-        
-        Solve(root->left,v);
-        v.push_back(root->val);
-        Solve(root->right,v);
+    struct newTreeNode {
+        int val;
+        int count;
+        newTreeNode *left;
+        newTreeNode *right;
+        newTreeNode(int x) : val(x) , count(1) {}
+    };
+    newTreeNode* T;
+    int helper(newTreeNode* tRoot, int k){
+        int leftCount = (tRoot->left == NULL)? 0 : tRoot->left->count;
+        if(k == leftCount + 1) return tRoot->val;
+        else if (k > leftCount + 1) return helper(tRoot->right, k - leftCount - 1);
+        else return helper(tRoot->left, k);
+    }
+    newTreeNode* buildtTree(TreeNode* root){
+        if(root == NULL) return NULL;
+        newTreeNode* tNode = new newTreeNode(root->val);
+        tNode->left = buildtTree(root->left);
+        tNode->right = buildtTree(root->right);
+        tNode->count +=  (tNode->left == NULL)? 0 : tNode->left->count;
+        tNode->count +=  (tNode->right == NULL)? 0 : tNode->right->count;
+        return tNode;
     }
     int kthSmallest(TreeNode* root, int k) {
-
-        vector<int> v;
-        Solve(root,v);
-        
-        return v[k-1];
+        T = buildtTree(root);
+        return helper(T, k);
     }
 };
