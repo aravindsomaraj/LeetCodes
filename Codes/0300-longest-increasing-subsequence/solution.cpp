307fd719e0f109c0bcc6ceab47ad56d9c2305ed6
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-27 09:05:40 +0000

    [LeetCode Sync] Runtime - 15 ms (77.46%), Memory - 10.3 MB (100.00%)

--- Codes/0300-longest-increasing-subsequence/solution.cpp
+++ Codes/0300-longest-increasing-subsequence/solution.cpp
@@ -2,72 +2,16 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
 
-        int n = nums.size();
-        vector<int> sub;
+        vector<int> act;
+
         for(int& i:nums)
         {
-            if(sub.empty() || sub[sub.size()-1] < i)
-                sub.push_back(i);
+            auto it = lower_bound(act.begin(),act.end(),i);
+            if(it!=act.end())
+                *it = i;
             else
-            {
-                auto it = lower_bound(sub.begin(),sub.end(),i);
-                *it=i;
-            }
+                act.push_back(i);
         }
-        return sub.size();
+        return act.size();
     }
 };
-
-// class Solution {
-// public:
-//     int lengthOfLIS(vector<int>& nums) {
-
-        // int n = nums.size();
-        // vector<int> sub;
-        // for(int& i:nums)
-        // {
-        //     if(sub.empty() || sub[sub.size()-1] < i)
-        //         sub.push_back(i);
-        //     else
-        //     {
-//                 int l=0;
-//                 int r=sub.size()-1;
-//                 while(l<r)
-//                 {
-//                     int mid = (l+r)/2;
-//                     if(i==sub[mid])
-//                     {
-//                         l=mid;break;
-//                     }
-//                     if(i<sub[mid])
-//                         r=mid;
-//                     else
-//                         l=mid+1;
-//                 }
-//                 sub[l]=i;
-//             }
-//         }
-//         return sub.size();
-//     }
-// };
-
-
-
-// class Solution {
-// public:
-//     int lengthOfLIS(vector<int>& nums) {
-
-//         int n = nums.size();
-//         vector<int> dp (n,1);
-
-//         for(int i=0;i<n;i++)
-//         {
-//             for(int j=0;j<i;j++)
-//             {
-//                 if(nums[j]<nums[i])
-//                     dp[i]=max(dp[i],1+dp[j]);
-//             }
-//         }
-//         return *max_element(dp.begin(),dp.end());
-//     }
-// };
