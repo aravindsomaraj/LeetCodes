c2208d3e989899fa88c4c5fce18ffc20bcc75184
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-06-27 10:37:42 +0000

    [LeetCode Sync] Runtime - 13 ms (5.55%), Memory - 10.4 MB (99.99%)

--- Codes/0328-odd-even-linked-list/solution.cpp
+++ Codes/0328-odd-even-linked-list/solution.cpp
@@ -12,47 +12,22 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
-        if(head==0)
-            return 0;
-
-        ListNode* curr = head;
-        int f=0;
-        ListNode *oddhead=0,*evenhead=0, *oddcur=0, *evecur=0;
-        while(curr!=0)
-        {
-            if((f++)%2==0)
-            {
-                if(evenhead==0)
-                {
-                    evenhead=curr;
-                    evecur=curr;
-                }
-                else
-                {
-                    evecur->next=curr;
-                    evecur=curr;
-                }
-            }
-            else
-            {
-                if(oddhead==0)
-                {
-                    oddhead=curr;
-                    oddcur=curr;
-                }
-                else
-                {
-                    oddcur->next=curr;
-                    oddcur=curr;
-                }
-            }
-            ListNode* temp = curr->next;
-            curr->next=0;
-            curr=temp;
-        }
-        evecur->next = oddhead;
-
-        return head;
+        if(!head || !head->next || !(head->next->next))
+            return head;
+        ListNode* one = head, *two = head->next, *two_head = head->next;
         
+
+        while(one && two)
+        {
+            one->next = two->next;
+            one=two;
+            two=two->next;
+        }
+        one = head;
+        while(one->next)
+            one = one->next;
+        one->next = two_head;
+        return head;
+
     }
 };
