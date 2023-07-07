24d6a4b746f19bca217a5915dbd0c093e960430e
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-07 14:37:54 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- Codes/problems/validate_binary_search_tree/solution.cpp
+++ Codes/problems/validate_binary_search_tree/solution.cpp
@@ -10,20 +10,19 @@
  * };
  */
 class Solution {
-    TreeNode* prev = NULL;
 public:
+    TreeNode* prev = NULL;
     bool isValidBST(TreeNode* root) {
+
         if(!root) return true;
-        
-        if(!isValidBST(root->left))
-            return false;
-        
+
+        if(!isValidBST(root->left)) return false;
         if(prev && prev->val >= root->val) return false;
         prev = root;
-        
-        if(!isValidBST(root->right))
-            return false;
-        
+
+        if(!isValidBST(root->right)) return false;
+
         return true;
+
     }
 };
