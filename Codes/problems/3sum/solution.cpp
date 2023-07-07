752e6ec0a2a813e82c1d932be71298e756805aaf
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-07 09:46:49 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- Codes/problems/3sum/solution.cpp
+++ Codes/problems/3sum/solution.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
-        set<vector<int>> set;
+
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        set<vector<int>> s;
         vector<vector<int>> output;
         for(int i=0;i<n-2;i++)
         {
-            int low = i+1,high=n-1;
+            int low = i+1, high = n-1;
             while(low<high)
             {
-                if(nums[low]+nums[high]+nums[i] < 0)
+                if(nums[i]+nums[low]+nums[high] < 0)
                     low++;
-                else if(nums[low]+nums[high]+nums[i] > 0)
+                else if(nums[i]+nums[low]+nums[high] > 0)
                     high--;
                 else
                 {
-                    set.insert({nums[i],nums[low],nums[high]});
-                    low++;
-                    high--;
+                    s.insert({nums[i],nums[low],nums[high]});
+                    low++,high--;
                 }
             }
         }
-        for(auto& v:set) output.push_back(v);
+
+        for(auto& it:s) output.push_back(it);
         return output;
     }
 };
