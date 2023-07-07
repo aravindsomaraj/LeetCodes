0634b34fb11a9c0161c0dba721ca296b83f4fe36
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-07 15:08:03 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- Codes/problems/number_of_islands/solution.cpp
+++ Codes/problems/number_of_islands/solution.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int c,m,n;
+    int m,n;
     void dfs(vector<vector<char>>& grid, int i, int j)
     {
-        if(i==m || j==n || i<0 || j<0 || grid[i][j]=='.' || grid[i][j]=='0')
+        if(i<0 || j<0 || i==m || j==n || grid[i][j]<='0')
             return;
         
-        grid[i][j]='.';
+        grid[i][j] = '0'-1;
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
         dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        c=0;
-        m=grid.size();n=grid[0].size();
-        for(int i=0;i<m;i++)
+        m = grid.size() , n = grid[0].size();
+        int color = 0;
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    c++; dfs(grid,i,j);
-                }
-            }
+            if(grid[i][j]=='1')
+                color++, dfs(grid,i,j);
         }
-        return c;
+        return color;
     }
 };
