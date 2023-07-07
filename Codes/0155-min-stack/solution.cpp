86b8cc1fb6e13b099a8680cd3b4f6da2983b4d8f
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-07 14:35:15 +0000

    [LeetCode Sync] Runtime - 23 ms (15.45%), Memory - 24.5 MB (5.55%)

--- Codes/0155-min-stack/solution.cpp
+++ Codes/0155-min-stack/solution.cpp
@@ -1,32 +1,31 @@
 class MinStack {
 public:
-    int a[(int)6e4];
+    int A[(int)6e4+1];
     int index,curr_min;
     MinStack() {
-        fill(a,a+(int)6e4,INT_MAX);
-        index=0;
-        curr_min=INT_MAX;
+        fill(A,A+(int)6e4+1,INT_MAX);
+        index = 2;
+        curr_min = INT_MAX;
     }
     
     void push(int val) {
         curr_min = min(val,curr_min);
-        a[index]=val;
-        a[index+1]=curr_min;
-        index+=2;
+        A[index] = val;
+        A[index+1] = curr_min;
+        index += 2;
     }
     
     void pop() {
-        if(index==2) curr_min=INT_MAX;
-        else curr_min = a[index-3];
-        index-=2;
+        curr_min = A[index-3];
+        index -= 2;
     }
     
     int top() {
-        return a[index-2];
+        return A[index-2];
     }
     
     int getMin() {
-        return a[index-1];
+        return A[index-1];
     }
 };
 
