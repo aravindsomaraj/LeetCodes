d0315b446e00fde99e754d8e32bda90b77252897
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-08 10:27:09 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

--- Codes/problems/spiral_matrix/solution.cpp
+++ Codes/problems/spiral_matrix/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
 
-        int m = matrix.size();int n=matrix[0].size();
-        int l=0,u=0,r=n-1,d=m-1,i=0,dir=0;
+        int m=mat.size(),n=mat[0].size();
+        int l=0,r=n-1,u=0,d=m-1,dir=0,i=0;
         vector<int> ans;
-        while(l<=r&&u<=d)
+        while(l<=r && u<=d)
         {
             switch(dir%4)
             {
-                case 0: for(i=l;i<=r;i++) ans.push_back(matrix[u][i]); u++; break;
-                case 1: for(i=u;i<=d;i++) ans.push_back(matrix[i][r]); r--; break;
-                case 2: for(i=r;i>=l;i--) ans.push_back(matrix[d][i]); d--; break;
-                case 3: for(i=d;i>=u;i--) ans.push_back(matrix[i][l]); l++; break;
+                case 0: for(i=l;i<=r;i++) ans.push_back(mat[u][i]); u++; break;
+                case 1: for(i=u;i<=d;i++) ans.push_back(mat[i][r]); r--; break;
+                case 2: for(i=r;i>=l;i--) ans.push_back(mat[d][i]); d--; break;
+                case 3: for(i=d;i>=u;i--) ans.push_back(mat[i][l]); l++; break;
             }
             dir++;
         }
