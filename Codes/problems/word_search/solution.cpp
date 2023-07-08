73db79cba812db3bbaade6b6bb25d8376307ded9
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-08 14:13:57 +0000

    Sync LeetCode submission - Word Search (cpp)

--- Codes/problems/word_search/solution.cpp
+++ Codes/problems/word_search/solution.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int m,n;string s;
-    bool dfs(vector<vector<char>>& board, int i, int j, int k)
+    int m,n;
+    bool dfs(vector<vector<char>>& board, int i, int j, int k, string s)
     {
-        if(k==s.size()) return true;
-        if(i==m || j==n || i<0 || j<0 || board[i][j]=='.' || board[i][j]!=s[k]) return false;
-
-        char ch = board[i][j];board[i][j]='.';
+        if(k==s.size())
+            return true;
+        if(i<0 || j<0 || i==m || j==n || board[i][j]=='.' || board[i][j]!=s[k])
+            return false;
+        
+        char ch = board[i][j];
+        board[i][j]='.';
         bool f = (
-            dfs(board,i+1,j,k+1) ||
-            dfs(board,i-1,j,k+1) ||
-            dfs(board,i,j+1,k+1) ||
-            dfs(board,i,j-1,k+1)
+            dfs(board,i+1,j,k+1,s) ||
+            dfs(board,i-1,j,k+1,s) ||
+            dfs(board,i,j-1,k+1,s) ||
+            dfs(board,i,j+1,k+1,s)
         );
         board[i][j]=ch;
         return f;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size(); n = board[0].size(); s=word;
-        for(int i=0;i<m;i++)
+        m = board.size(), n=board[0].size();
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(board[i][j]==word[0])
-                {
-                    if(dfs(board,i,j,0)) return true;
-                }
-            }
+            if(board[i][j]==word[0])
+                if(dfs(board,i,j,0,word)) return true;
         }
         return false;
     }
