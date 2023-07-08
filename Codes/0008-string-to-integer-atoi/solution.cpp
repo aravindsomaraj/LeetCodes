54cf5dd0180880e8831c68866952d9cabd97b4d6
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-08 10:18:35 +0000

    [LeetCode Sync] Runtime - 4 ms (27.89%), Memory - 7.1 MB (100.00%)

--- Codes/0008-string-to-integer-atoi/solution.cpp
+++ Codes/0008-string-to-integer-atoi/solution.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        
         int n = s.size();
-        int i = 0;
-        while(i<n && s[i]==' ')
-            i++;
-        
-        int f = 0;
-        if(s[i]=='-') f = -1;
-        else if(s[i]=='+') f = 1;
-        !f? f=1: i++;
-        int num=0;
+        int f=0;
+        int i=0;
+        while(i<n && s[i]==' ') i++;
+
+        if(s[i]=='-') f=-1;
+        else if(s[i]=='+') f=1;
+        !f? f=1 : i++;
+
+        int num = 0;
         while(i<n && isdigit(s[i]))
         {
-            int d = s[i++]-'0';
-            if(num > INT_MAX/10 || (num==INT_MAX/10 && d>7))
+            int d = s[i++] - '0';
+            if(num > (INT_MAX)/10 || (num*10 > INT_MAX-d))
                 return f>0? INT_MAX:INT_MIN;
-            
             num = num*10 + d;
         }
         return f*num;
-
     }
 };
