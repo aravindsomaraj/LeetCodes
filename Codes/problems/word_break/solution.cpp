b20496e7dc70bd9ebf54e45302b888e35b47e024
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-08 09:06:27 +0000

    Sync LeetCode submission - Word Break (cpp)

--- Codes/problems/word_break/solution.cpp
+++ Codes/problems/word_break/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-       int n = s.size();
-       vector<bool> dp(n+1,false);
-
-       dp[0] = true;
-       for(int i=0;i<n;i++)
-       {
-            if(dp[i]) for(auto str:wordDict)
-                if(s.substr(i,str.size())==str) dp[i+str.size()] = true;
-       }
-       return dp[n];
+        
+        int n=s.size();
+        vector<int> dp (n+1);
+        dp[0] = 1;
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]) for(string& str:wordDict)
+            {
+                if(s.substr(i,str.size())==str) dp[i+str.size()] = 1;
+            }
+        }
+        return dp[n];
     }
 };
