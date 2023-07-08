d6cb982bf8b3597e4e6f93442e3278a2ff130fe9
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-08 12:45:35 +0000

    [LeetCode Sync] Runtime - 5 ms (13.08%), Memory - 6.5 MB (100.00%)

--- Codes/0062-unique-paths/solution.cpp
+++ Codes/0062-unique-paths/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    int dfs(int m, int n, int i, int j, vector<vector<int>>& M)
+    {
+        if(i<0 || j<0 || i==m || j==n) return 0;
+        if(M[i][j]!=-1) return M[i][j];
+
+        return M[i][j]=dfs(m,n,i+1,j,M)+dfs(m,n,i,j+1,M);
+    }
     int uniquePaths(int m, int n) {
-
-        vector<vector<int>> dp (m,vector<int>(n,0));
-        dp[m-1][n-1]=1;
-
-        for(int i=m-1;i>=0;i--) for(int j=n-1;j>=0;j--)
-                if(!dp[i][j])
-                {
-                    if(i<m-1) dp[i][j]+=dp[i+1][j];
-                    if(j<n-1) dp[i][j]+=dp[i][j+1];
-                }
-        return dp[0][0];
+        
+        vector<vector<int>> M (m,vector<int>(n,-1));
+        M[m-1][n-1]=1;
+        return dfs(m,n,0,0,M);
     }
 };
