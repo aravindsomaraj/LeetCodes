b0cd2cf16d61c2c55855cfe788f7b8b55a563679
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-09 08:23:31 +0000

    [LeetCode Sync] Runtime - 331 ms (11.51%), Memory - 116.9 MB (100.00%)

--- Codes/0295-find-median-from-data-stream/solution.cpp
+++ Codes/0295-find-median-from-data-stream/solution.cpp
@@ -2,28 +2,22 @@ class MedianFinder {
 public:
     priority_queue<int> left;
     priority_queue<int,vector<int>,greater<int>> right;
-    
     void addNum(int num) {
-        if(left.empty() || left.top()>num ){
-            left.push(num);
-        }
-        else{
-            right.push(num);
-        }
-        if(left.size()>right.size()+1){
+        if(left.empty() || left.top()>num) left.push(num);
+        else    right.push(num);
+
+        if(left.size() > right.size()+1) {
             right.push(left.top());
             left.pop();
         }
-        else if(right.size()>left.size()+1){
+        else if(right.size() > left.size()+1) {
             left.push(right.top());
             right.pop();
         }
     }
     
     double findMedian() {
-        if(left.size()==right.size())
-        return (left.top()+right.top())/2.00000;
-        return left.size()>right.size()?left.top():right.top();
+        return left.size()==right.size()? (left.top()+right.top())/2.0000 : left.size()>right.size()? left.top() : right.top();
     }
 };
 
