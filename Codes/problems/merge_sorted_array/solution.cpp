ad888bef94a55a65306211bc6d4ec3128ca2503a
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 15:14:52 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

--- Codes/problems/merge_sorted_array/solution.cpp
+++ Codes/problems/merge_sorted_array/solution.cpp
@@ -4,12 +4,12 @@ public:
 
         for(int i=m+n-1;i>=0;i--)
         {
-            cout << i;
-            if(m>=1 && n>=1 && nums1[m-1]>nums2[n-1] || (m>=1 && n==0))
-                nums1[i]=nums1[m---1];
+            cout << i << " ";
+            if(m>0 && n>0 && nums1[m-1] >= nums2[n-1] || (m>=1 && n==0))
+                nums1[i] = nums1[m---1];
             else
-                nums1[i]=nums2[n---1];
-            cout << " " << nums1[i] << endl;
+                nums1[i] = nums2[n---1];
         }
+        
     }
 };
