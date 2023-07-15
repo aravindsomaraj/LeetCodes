bc68c7864c3154f190a8f7fe8efceca789cc1b2f
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 18:55:37 +0000

    [LeetCode Sync] Runtime - 3 ms (40.34%), Memory - 7.3 MB (99.97%)

--- Codes/0412-fizz-buzz/solution.cpp
+++ Codes/0412-fizz-buzz/solution.cpp
@@ -3,14 +3,16 @@ public:
     vector<string> fizzBuzz(int n) {
 
         vector<string> ans (n,"");
-        for(int i=1; i<=n; i++)
-            ans[i-1]=to_string(i);
-        for(int i=3;i<=n;i+=3)
-            ans[i-1]="Fizz";
-        for(int i=5;i<=n;i+=5)
-            ans[i-1]="Buzz";
-        for(int i=15;i<=n;i+=15)
-            ans[i-1]="FizzBuzz";
+
+        for(int i=1;i<=n;i++)
+        {
+            if(!(i%3))
+                ans[i-1]+="Fizz";
+            if(!(i%5))
+                ans[i-1]+="Buzz";
+            if(ans[i-1]=="")
+                ans[i-1]=to_string(i);
+        }
         return ans;
     }
 };
