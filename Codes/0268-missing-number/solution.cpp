596aa1e27cd5d16b3dade52cc312bd9c0a509651
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 18:14:20 +0000

    [LeetCode Sync] Runtime - 23 ms (8.89%), Memory - 18 MB (100.00%)

--- Codes/0268-missing-number/solution.cpp
+++ Codes/0268-missing-number/solution.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-        int x = 0;int i=0;
-        for(i=0; i<nums.size();i++)
-        {
-            x = x ^ i ^ nums[i];
-        }
-        x ^= i;
-        return x;
+        int x = 0;
+        for(int i=0;i<nums.size();i++) x = x^i^nums[i];
+        return x^nums.size();
     }
 };
