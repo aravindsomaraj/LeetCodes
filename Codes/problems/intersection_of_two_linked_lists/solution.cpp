01f2df6590fd622c4ec0fbe8f824cf93660992a9
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 17:26:08 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (cpp)

--- Codes/problems/intersection_of_two_linked_lists/solution.cpp
+++ Codes/problems/intersection_of_two_linked_lists/solution.cpp
@@ -9,10 +9,12 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *a = headA, *b = headB;
-        while (a != b) {
-            a = !a ? headB : a->next;
-            b = !b ? headA : b->next;
+
+        ListNode* a = headA, *b = headB;
+        while(a!=b)
+        {
+            a = !a? headB : a->next;
+            b = !b? headA : b->next;
         }
         return a;
     }
