4210e8bdffefd29d7b98ec17224187c4322203eb
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 08:58:47 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- Codes/problems/roman_to_integer/solution.cpp
+++ Codes/problems/roman_to_integer/solution.cpp
@@ -3,18 +3,13 @@ public:
     int romanToInt(string s) {
 
         unordered_map<char,int> mp = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-        int num=0;
-        for(int i=0;s[i]!='\0';i++)
+        int num = 0;
+        for(int i=0;i<s.size();i++)
         {
-            if(i<s.size()-1 && mp[s[i]]<mp[s[i+1]])
-            {
-                num = num + (mp[s[i+1]] - mp[s[i]]);
-                i++;
-            }
+            if(i<s.size()-1 && mp[s[i+1]] > mp[s[i]])
+                num += mp[s[i+1]] - mp[s[i++]];
             else
-            {
                 num += mp[s[i]];
-            }
         }
         return num;
     }
