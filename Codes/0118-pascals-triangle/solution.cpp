1defd8a17e5456938b42ce4d1395deb22de62031
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 15:32:01 +0000

    [LeetCode Sync] Runtime - 5 ms (35.15%), Memory - 6.6 MB (100.00%)

--- Codes/0118-pascals-triangle/solution.cpp
+++ Codes/0118-pascals-triangle/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-	vector<vector<int>> ret;
-	for (int i = 0; i < numRows; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
-		}
-		ret.push_back(row);
-	}
-	return ret;
-}
+
+        vector<vector<int>> ret;
+        for(int i=1;i<=numRows;i++)
+        {
+            vector<int> res (i,1);
+            for(int j=1;j<i-1;j++)
+            {
+                res[j] = ret[i-2][j] + ret[i-2][j-1]; 
+            }
+            ret.push_back(res);
+        }
+        return ret;
+    }
 };
