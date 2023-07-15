f31e8e5573602a1f457a5d1305a5c6c34f12d089
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-15 09:14:55 +0000

    [LeetCode Sync] Runtime - 8 ms (10.11%), Memory - 9.1 MB (100.00%)

--- Codes/0014-longest-common-prefix/solution.cpp
+++ Codes/0014-longest-common-prefix/solution.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
 
-        if(strs.size()==1)
-            return strs[0];
-        
-        for(int i=0; i<strs[0].size();i++)
+        std::ios_base::sync_with_stdio(NULL);
+        std::cin.tie(NULL);
+        int m = strs.size();
+        int n = strs[0].size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<strs.size();j++)
+            for(int j=1;j<m;j++)
             {
-                if(strs[j][i]=='\0' || strs[j][i]!=strs[0][i])
+                if(strs[j][i]=='\0' || strs[0][i]!=strs[j][i])
                     return strs[0].substr(0,i);
             }
         }
