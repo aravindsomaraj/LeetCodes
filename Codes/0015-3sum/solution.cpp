1181da53799ca4bec78c2263ddbe64713d8d07d5
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-25 20:08:17 +0000

    [LeetCode Sync] Runtime - 141 ms (40.21%), Memory - 23.9 MB (100.00%)

--- Codes/0015-3sum/solution.cpp
+++ Codes/0015-3sum/solution.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
+        vector<vector<int>> result;
         sort(nums.begin(),nums.end());
-        int n = nums.size();
-        set<vector<int>> s;
-        vector<vector<int>> output;
+        int n=nums.size();
+        int l,r;
         for(int i=0;i<n-2;i++)
         {
-            int low = i+1, high = n-1;
-            while(low<high)
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+
+            l=i+1;
+            r=n-1;
+            while(l<r)
             {
-                if(nums[i]+nums[low]+nums[high] < 0)
-                    low++;
-                else if(nums[i]+nums[low]+nums[high] > 0)
-                    high--;
+                int sum=nums[l]+nums[r]+nums[i];
+
+                if(sum<0)
+                l++;
+
+                else if(sum>0)
+                r--;
+
                 else
-                {
-                    s.insert({nums[i],nums[low],nums[high]});
-                    low++,high--;
-                }
+                { 
+                    result.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    while(l<n-1 && nums[l]==nums[l-1]) l++;
+                    r--;
+                    while(r>l && nums[l]==nums[r+1]) r--;
+                };
             }
         }
-
-        for(auto& it:s) output.push_back(it);
-        return output;
+        return result;
     }
 };
