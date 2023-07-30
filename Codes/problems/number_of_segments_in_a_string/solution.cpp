f38f8ba9cff10622bd40f1116aa52eeddb0fb61f
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-07-30 20:01:17 +0000

    Sync LeetCode submission - Number of Segments in a String (cpp)

---
+++ Codes/problems/number_of_segments_in_a_string/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        
+        stringstream ss(s);
+        string word;
+ 
+        int count = 0;
+        while (ss >> word)
+            count++;
+        return count;
+    }
+};
