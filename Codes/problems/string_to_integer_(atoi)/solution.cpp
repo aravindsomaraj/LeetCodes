83823a6503fd44404241823cb195cdcc61f23822
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-15 12:07:48 +0000

    Sync LeetCode submission - String to Integer (atoi) (cpp)

--- Codes/problems/string_to_integer_(atoi)/solution.cpp
+++ Codes/problems/string_to_integer_(atoi)/solution.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.size();
-        int f=0;
-        int i=0;
+
+        int n = s.size(), f=0, i=0;
         while(i<n && s[i]==' ') i++;
 
         if(s[i]=='-') f=-1;
@@ -13,10 +12,11 @@ public:
         int num = 0;
         while(i<n && isdigit(s[i]))
         {
-            int d = s[i++] - '0';
-            if(num > (INT_MAX)/10 || (num*10 > INT_MAX-d))
-                return f>0? INT_MAX:INT_MIN;
+            int d = s[i]-'0';
+            if(num > INT_MAX/10 || (num*10 > INT_MAX-d))
+                return f>0? INT_MAX : INT_MIN;
             num = num*10 + d;
+            i++;
         }
         return f*num;
     }
