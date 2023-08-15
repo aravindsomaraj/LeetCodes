09dae90bef26169588877cfcefe22fbdbf2e150e
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-15 11:56:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- Codes/0007-reverse-integer/solution.cpp
+++ Codes/0007-reverse-integer/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
+        int f = x>0? 1 : -1;
 
-        if(!x) return 0;
-
-        int y = abs(x),rev=0;
-
-        while(y>0)
+        int temp = abs(x), rev = 0;
+        while(temp>0)
         {
-            int d=y%10;
-            if(rev <= (INT_MAX)/10)
-                rev = rev*10 + d;
-            else return 0;
-            y/=10;
+            int d = temp%10;
+            if(rev > INT_MAX/10)
+                return 0;
+            rev = rev*10 + d;
+            temp/=10;
         }
-        x = x>0? rev:-rev;
-        return x;
+        return rev*f;
     }
 };
