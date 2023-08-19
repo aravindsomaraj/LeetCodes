85ecfae4cec01032d964e50ef09426d758bc74e6
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-19 13:30:03 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- Codes/problems/palindrome_number/solution.cpp
+++ Codes/problems/palindrome_number/solution.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        if(x<0)
-            return false;
-        vector<int> ans;
-        while(x)
+        if(x<0) return false;
+        int rev=0;
+        int temp = x;
+        while(temp)
         {
-            ans.push_back(x%10);
-            x/=10;
-        }
-        
-        int l=0, r=ans.size()-1;
-        while(l<r)
-        {
-            if(ans[l]!=ans[r])
+            int d = temp%10;
+            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && d>7))
                 return false;
-            l++;
-            r--;
+            rev = rev*10 + d;
+            temp/=10;
         }
-        
-        return true;
-        
+        return rev==x;
     }
 };
