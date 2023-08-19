e6b1409278238112e95ebd81588db0168837e1d0
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-19 11:24:12 +0000

    [LeetCode Sync] Runtime - 3 ms (99.81%), Memory - 17 MB (100.00%)

--- Codes/0136-single-number/solution.cpp
+++ Codes/0136-single-number/solution.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& arr) {
-        int n = arr.size(); // extracting the size of the array
-        
-        // traverse from the array
-        for(int i = 0; i < n - 1; i++)
-        {
-            arr[i + 1] = arr[i] ^ arr[i + 1]; // (prev answer xor current index)
-        }
-        
-        return arr[n- 1]; // return left over element
+    int singleNumber(vector<int>& nums) {
+
+        int x = 0;
+        for(int& i:nums)
+            x = x^i;
+        return x;
     }
 };
