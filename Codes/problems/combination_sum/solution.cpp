c2bf80588259fb68770162f0d4a71ba76178771c
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-20 19:48:36 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- Codes/problems/combination_sum/solution.cpp
+++ Codes/problems/combination_sum/solution.cpp
@@ -8,12 +8,14 @@ public:
             ans.push_back(temp);
             return;
         }
-        if(target < 0 || i==candidates.size()) return;
+        if(target < 0) return;
 
-        solve(temp,i+1,candidates,target);
-        temp.push_back(candidates[i]);
-        solve(temp,i,candidates,target-candidates[i]);
-        temp.pop_back();
+        for(int j=i;j<candidates.size();j++)
+        {
+            temp.push_back(candidates[j]);
+            solve(temp,j,candidates,target-candidates[j]);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
