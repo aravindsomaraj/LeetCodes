fa75fe17a149dfe2062f966ec2af6ca460aa3b64
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-08-23 08:34:56 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

--- Codes/problems/minimum_path_sum/solution.cpp
+++ Codes/problems/minimum_path_sum/solution.cpp
@@ -1,29 +1,16 @@
+#define maxo INT_MAX-500
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        vector<vector<int>> mem (m,vector<int> (n,-1));
-        
-        return dp(grid, m-1, n-1, mem);
-        
-    }
-    
-    int dp(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& mem)
-    {
-        if(i==0 && j==0)
-            return grid[0][0];
-        
-        if(i<0 || j<0)
-            return INT_MAX;
-        
-        if(mem[i][j]!=-1)
-            return mem[i][j];
-        
-        mem[i][j] = min(dp(grid,i,j-1,mem),dp(grid,i-1,j,mem)) + grid[i][j];
-        
-        return mem[i][j];
+
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp (m,vector<int>(n,0));
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<m;i++) dp[i][0] = grid[i][0] + dp[i-1][0];
+        for(int j=1;j<n;j++) dp[0][j] = grid[0][j] + dp[0][j-1];
+        for(int i=1;i<m;i++)
+            for(int j=1;j<n;j++)
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+        return dp[m-1][n-1];
     }
 };
