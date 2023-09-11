a3c62941e03e4914ecf87563de1e00928a912617
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-09-11 14:09:13 +0000

    [LeetCode Sync] Runtime - 235 ms (5.51%), Memory - 146.7 MB (14.32%)

--- Codes/0111-minimum-depth-of-binary-tree/solution.cpp
+++ Codes/0111-minimum-depth-of-binary-tree/solution.cpp
@@ -12,15 +12,14 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        
-        if(root==NULL)
-            return 0;
-        cout << root->val << " ";
-        if(root->left!=NULL && root->right!=NULL )
-            return min(1+minDepth(root->left),1+minDepth(root->right));
-        else if(root->left == NULL)
-            return 1+minDepth(root->right);
-        else
+
+        if(!root) return 0;
+
+        if(root->left && root->right)
+            return 1+min(minDepth(root->left),minDepth(root->right));
+        else if(root->left)
             return 1+minDepth(root->left);
+        return 1+minDepth(root->right);
+        
     }
 };
