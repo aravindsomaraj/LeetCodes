c957dd96e42933ed76ef822c6a1bb5c7b3ff3e17
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-09-11 14:06:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.9 MB (10.66%)

--- Codes/0104-maximum-depth-of-binary-tree/solution.cpp
+++ Codes/0104-maximum-depth-of-binary-tree/solution.cpp
@@ -11,11 +11,24 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
+    // int a,b;
+    // int helper(TreeNode* root)
+    // {        
+    //     if(root == NULL) return 0;
 
+    //     if(root->val == a || root->val == b)
+    //         return 1;
+        
+    //     int l = helper(root->left);
+    //     int r = helper(root->right);
+    
+    //     if(l && r) return (l+r);
+    //     else if(l || r) return max(l,r)+1;
+    //     return 0;
+    // }
+    int maxDepth(TreeNode* root) {
         if(!root) return 0;
 
-        return 1+max(maxDepth(root->left),maxDepth(root->right));
-        
+        return 1+max(maxDepth(root->left),maxDepth(root->right));        
     }
 };
