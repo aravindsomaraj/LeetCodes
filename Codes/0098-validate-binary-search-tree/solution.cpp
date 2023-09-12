f143cf41f0e305a053188636f3cad1a04aae3f67
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-09-12 09:27:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.7 MB (7.18%)

--- Codes/0098-validate-binary-search-tree/solution.cpp
+++ Codes/0098-validate-binary-search-tree/solution.cpp
@@ -11,18 +11,15 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
+    TreeNode* prev = nullptr;
     bool isValidBST(TreeNode* root) {
-
         if(!root) return true;
-
+        
         if(!isValidBST(root->left)) return false;
         if(prev && prev->val >= root->val) return false;
         prev = root;
-
         if(!isValidBST(root->right)) return false;
 
         return true;
-
     }
 };
