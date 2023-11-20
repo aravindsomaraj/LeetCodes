db2ce4e514a17b5c6cee0569be7e2a67396741d6
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2023-11-20 20:10:11 +0000

    [LeetCode Sync] Runtime - 40 ms (51.16%), Memory - 28.5 MB (100.00%)

--- Codes/0561-array-partition/solution.cpp
+++ Codes/0561-array-partition/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        int sum = 0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i += 2) {
-            sum += nums[i];
-        }
+        for(int i=1; i<nums.size(); i+=2)
+            nums[i] = nums[i-1];
         
-        return sum;
+        int ans = 0;
+        for(auto i: nums)
+            ans += i;
+        
+        return ans/2;
     }
 };
