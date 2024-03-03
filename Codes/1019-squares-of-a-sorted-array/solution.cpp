59307c7774fd52b7a84e006b4254ac3f1e946723
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2024-03-03 21:36:14 +0000

    [LeetCode Sync] Runtime - 16 ms (89.59%), Memory - 29.5 MB (46.76%)

--- Codes/1019-squares-of-a-sorted-array/solution.cpp
+++ Codes/1019-squares-of-a-sorted-array/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
     public:
         vector<int> sortedSquares(vector<int>& nums) {
-                vector<int> squares(nums.size());
-                        transform(nums.begin(), nums.end(), squares.begin(), [](int num) { return num * num; });
-                                sort(squares.begin(), squares.end());
-                                        return squares;   
-                                            }
-                                            };
-
+            vector<int> squares(nums.size());
+            transform(nums.begin(), nums.end(), squares.begin(), [](int num) { return num * num; });
+            sort(squares.begin(), squares.end());
+            return squares;   
+    }
+};
 
