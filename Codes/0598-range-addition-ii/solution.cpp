0a12311b82f59fee7ecbe860f18889245454c2d8
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2024-10-13 16:58:09 +0000

    [LeetCode Sync] Runtime - 8 ms (58.72%), Memory - 14.3 MB (96.48%)

---
+++ Codes/0598-range-addition-ii/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int minRow = m, minCol = n;
+        for(auto &v:ops)
+        {
+            minRow = min(minRow,v[0]);
+            minCol = min(minCol,v[1]);
+        }
+        return minRow*minCol;
+    }
+};
