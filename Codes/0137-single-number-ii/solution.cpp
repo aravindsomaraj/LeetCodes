a6044ce9c5d7744adabb669a88a827eb6e7a5515
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2024-10-16 20:37:41 +0000

    [LeetCode Sync] Runtime - 15 ms (5.48%), Memory - 12.2 MB (59.16%)

---
+++ Codes/0137-single-number-ii/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for (const int num : nums) {
+            ones ^= (num & ~twos);
+            twos ^= (num & ~ones);
+            cout << ones << " " << twos << endl;
+        }
+
+        return ones;
+    }
+};
