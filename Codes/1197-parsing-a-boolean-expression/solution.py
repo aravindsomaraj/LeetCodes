2c444c4c3d87bd201439de07250dda853cb6d11f
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2024-10-20 09:55:08 +0000

    [LeetCode Sync] Runtime - 75 ms (14.74%), Memory - 20.6 MB (5.40%)

---
+++ Codes/1197-parsing-a-boolean-expression/solution.py
@@ -0,0 +1,3 @@
+class Solution(object):
+    def parseBoolExpr(self, S, t=True, f=False):
+        return eval(S.replace('!', 'not |').replace('&(', 'all([').replace('|(', 'any([').replace(')', '])'))
