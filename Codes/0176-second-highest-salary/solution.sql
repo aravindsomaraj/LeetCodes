90d42e0dd98cd015f1281b37273a5ae93897e261
Aravind Somaraj <104314434+aravindsomaraj@users.noreply.github.com>
2024-10-22 17:50:03 +0000

    [LeetCode Sync] Runtime - 232 ms (47.97%), Memory - 0B (100.00%)

---
+++ Codes/0176-second-highest-salary/solution.sql
@@ -0,0 +1,8 @@
+WITH highest_salary AS (
+    SELECT MAX(salary) AS salary
+    FROM Employee
+)
+
+SELECT MAX(salary) AS "SecondHighestSalary" 
+FROM Employee
+WHERE salary < (SELECT salary FROM highest_salary);
